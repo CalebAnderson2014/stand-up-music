ccde72e4be69d01dedde7553b6f96c5c659b0650
Caleb Anderson <caleb.anderson130@gmail.com>
2016-09-08 18:53:40 -0500

    6:56

--- client/components/CardWindow.js
+++ client/components/CardWindow.js
@@ -3,13 +3,13 @@ import schedule from 'node-schedule';
 
 export default class StandUp extends Component{
   componentWillMount() {
-    var j = schedule.scheduleJob({hour: 18, minute: 45}, function(){
+    var j = schedule.scheduleJob({hour: 18, minute: 56}, function(){
       console.log('Playing music');
       playMusic();
     });
   }
   render() {
-    return <h3> Stand Up for your rights at 3:15 </h3>
+    return <h3> Stand Up for your rights at 6:56 </h3>
   }
 }
 
