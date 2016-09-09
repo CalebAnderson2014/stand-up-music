47fd53d590d36524e9f3f7c58f1332b646041771
Caleb Anderson <caleb.anderson130@gmail.com>
2016-09-08 19:25:24 -0500

    time set to 3:15

--- client/components/StandUp.js
+++ client/components/StandUp.js
@@ -3,7 +3,7 @@ import schedule from 'node-schedule';
 
 export default class StandUp extends Component{
   componentWillMount() {
-    var j = schedule.scheduleJob({hour: 19, minute: 16}, function(){
+    var j = schedule.scheduleJob({hour: 15, minute: 15}, function(){
       console.log('Playing music');
       playMusic();
     });
