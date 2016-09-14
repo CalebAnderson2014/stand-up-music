44f27ca24ad0f3948d93c518472219b3c10fef36
Caleb Anderson <caleb.anderson130@gmail.com>
2016-09-14 17:32:25 -0500

    added time select functionality

--- client/components/StandUp.js
+++ client/components/StandUp.js
@@ -1,20 +1,61 @@
 import React, {Component} from 'react';
 import schedule from 'node-schedule';
 
+var j;
+
 export default class StandUp extends Component{
-  componentWillMount() {
-    var j = schedule.scheduleJob({hour: 15, minute: 15}, function(){
-      console.log('Playing music');
-      playMusic();
-    });
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hour: 0,
+      minute: 0
+    }
+
+    handleTimeChange = handleTimeChange.bind(this)
+    handleSubmit = handleSubmit.bind(this)
+  }
+
+  componentDidUpdate() {
+
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={e => {
+          e.preventDefault()
+          handleSubmit()
+        }}>
+          <label> Hour : </label>
+          <input type="time" onChange={e => handleTimeChange(e.target.value, 'h')}/>
+          <button type="submit"> Save changes! </button>
+        </form>
+      </div>
+    )
   }
 }
 
+function handleTimeChange(time, mode) {
+  let newTime = time.split(':')
+
+  this.setState({ hour: Number(newTime[0]), minute: Number(newTime[1]) })
+}
 function playMusic() {
   let marley = new Audio('GetUpStandUp.mp3');
 
   marley.play();
 }
 
+
+function handleSubmit() {
+  if(j) {
+    j.cancel()
+  }
+
+  j = schedule.scheduleJob({ hour: this.state.hour, minute: this.state.minute }, function(){
+    console.log('Playing music');
+    playMusic();
+  });
+}
 
 
--- server/main.js
+++ server/main.js
@@ -2,10 +2,7 @@ var express = require('express');
 var path = require('path');
 var browserify = require('browserify-middleware');
 var app = express();
-var schedule = require('node-schedule');
 
-var date = new Date(2016, 9, 21, 5, 30, 0);
-// '41 17 * * 1-5 * (41 17 * * Mon-Fri *)'
 
 app.use(express.static(path.join(__dirname, "../client/public")));
 
